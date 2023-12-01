a230bf5d05f7f43a150190ed534f1682762d03bb
Vidya Sinta Billkis <vidya.sinta21@students.unila.ac.id>
2023-12-01 10:47:33 +0700

    update laman admin

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,3 +8,7 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 $routes->get('/login', 'Home::login');
 $routes->get('/register', 'Home::register');
+$routes->get('/admin', 'Home::dashboard_admin');
+$routes->get('/admin/gejala', 'Home::admin_datagejala');
+$routes->get('/admin/kerusakan', 'Home::admin_datakerusakan');
+$routes->get('/admin/solusi', 'Home::admin_datasolusi');
